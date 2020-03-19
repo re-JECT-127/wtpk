60244f808d7052868dfac82b0c831589c2f9ae2e
reject <reject@pop-os.localdomain>
2020-03-19 15:41:56 +0200

    Files

---
+++ server-two/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    'name': 'Franklin',
+    'age': 6,
+    'weight': 5,
+  };
+  res.json(cat);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
